8835997c4101417df20388d8b3cd5aa4ddd68d28
Carlos Mejia <ccmm.05.08@gmail.com>
2023-07-27 21:01:57 -0500

    Solve the repeat string exercise from JS exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(givenString, numberTimes) {
+    let fullString = '';
+    if (numberTimes < 0) {
+        return 'ERROR';
+    }
+    while (numberTimes > 0) {
+        fullString += givenString;
+        numberTimes--;
+    }
+    return fullString;
 };
 
 // Do not edit below this line
